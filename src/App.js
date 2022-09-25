0d0cab0111f7e54aa9e07cf36a9d58b4cde1b72f
kaxada <ekaxada@gmail.com>
2022-09-25 09:56:16 +0300

    inital commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+const App = () => {
+    return (
+        <h2>Badging Website</h2>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const webpack = require("webpack")
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "[name].bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'build'),
+        },
+        client: {
+            logging: 'info',
+            overlay: true,
+            reconnect: 2,
+        },
+        compress: true,
+        port: 5050,
+        hot: true,
+
+    },
+    devtool: "source-map"
+}
