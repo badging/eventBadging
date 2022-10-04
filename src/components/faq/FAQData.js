99f86e836a06ff5118ef7663f56af8a9a43fed3e
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-10-04 16:40:47 +0400

    Created the landing page Home

---
+++ src/components/faq/FAQData.js
@@ -0,0 +1,17 @@
+export const FAQData = [
+  {
+    question: "What requrements are needed before i apply for a badge ?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eros, mi ultricies integer erat dictum. Nunc ultrices sit justo bibendum nibh. Eget sed in egestas accumsan sit porta.",
+  },
+  {
+    question: "What requrements are needed before i apply for a badge ?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eros, mi ultricies integer erat dictum. Nunc ultrices sit justo bibendum nibh. Eget sed in egestas accumsan sit porta.",
+  },
+  {
+    question: "What requrements are needed before i apply for a badge ?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eros, mi ultricies integer erat dictum. Nunc ultrices sit justo bibendum nibh. Eget sed in egestas accumsan sit porta.",
+  },
+];
