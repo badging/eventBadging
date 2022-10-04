23216945fb363b98eedfee97f5b95c3c99ddc822
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-10-04 23:59:03 +0400

    Added the required images
    Signed-off-by: Chimuanya Nnaji <nnajichimuanya50@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { Header, Footer } from "./components";
 
 const App = () => {
-
   return (
     <>
       <Header />
       <h1>Badging Website</h1>
       <Footer />
     </>
-  )
+  );
 };
 
 export default App;
