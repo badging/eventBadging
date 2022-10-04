e5637d604652901e05759cc755b33cba5a073717
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-10-04 23:04:32 +0400

    Worked on the landingPage

---
+++ src/components/testimonial/testimonialData.js
@@ -0,0 +1,27 @@
+import girl from "../../../assets/images/girl.png";
+import man from "../../../assets/images/man.png";
+import guy from "../../../assets/images/guy.png";
+
+export const testimonialData = [
+  {
+    image: girl,
+    name: "Yaire Tally",
+    title: "Co-founder Bitzu",
+    message:
+      "Lorem ipsum dolor sit amet, ectetur adipiscing elit. Volutpat, nunc, ibero odio lacinia faucibus mus. Egetattis in purus, erat. Urna, tortor feugiat aliquam semper. ",
+  },
+  {
+    image: man,
+    name: "Yaire Tally",
+    title: "Co-founder Bitzu",
+    message:
+      "Lorem ipsum dolor sit amet, ectetur adipiscing elit. Volutpat, nunc, ibero odio lacinia faucibus mus. Egetattis in purus, erat. Urna, tortor feugiat aliquam semper. ",
+  },
+  {
+    image: guy,
+    name: "Yaire Tally",
+    title: "Co-founder Bitzu",
+    message:
+      "Lorem ipsum dolor sit amet, ectetur adipiscing elit. Volutpat, nunc, ibero odio lacinia faucibus mus. Egetattis in purus, erat. Urna, tortor feugiat aliquam semper. ",
+  },
+];
