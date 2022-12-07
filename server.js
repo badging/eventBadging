c9256edd962e7f66ae4597f8b37e03fee737e6aa
Tochukwu <tochukwuodoeme@gmail.com>
2022-12-07 20:19:52 +0100

    remove test

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 app.use(express.static("build"));
-app.use('/', (req, res) => {
-  res.sendFile(__dirname, 'views/index.html')
-})
 app.listen(process.env.PORT, function () {
   console.info(`website running on Port: ${process.env.PORT}`);
 });
