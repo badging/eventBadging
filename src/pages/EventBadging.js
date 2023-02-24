ff76bf5e0d74a1049db6a93e22bfb64d96f9687d
c3c1l1a <cesswangumukima@gmail.com>
2023-02-24 23:06:51 +0800

    Add resposive hero section

--- src/pages/EventBadging.js
+++ src/pages/EventBadging.js
@@ -1,10 +1,32 @@
 import { Header, Footer } from "../layouts";
+import "../assets/styles/event-badging.css";
+import heroBg from "../assets/images/event-badging/bg/event-badging-hero-bg.png";
 
 const EventBadging = () => {
     return (
         <>
             <Header />
-            <h1>Event Badging</h1>
+            <div className="event-badging-hero-section" style={{
+                backgroundImage: `url(${heroBg})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize:  "cover",
+                backgroundPosition: "center center"
+            }}>
+                <h1>Event Badging</h1>
+                <p>
+                    D&I Badging program aims to increase understanding of the 
+                    open-source project and event practices that encourage 
+                    greater diversity and wi
+                </p>
+                <div className="button-container">
+                    <button className="in-person-button">
+                        In-person Event
+                    </button>
+                    <button className="virtual-button">
+                        Virtual Event
+                    </button>
+                </div>
+            </div>
             <Footer />
         </>
     )
