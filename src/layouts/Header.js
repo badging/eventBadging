7e9c3be40c085fa7e1f093503928645ed43abec4
c3c1l1a <cesswangumukima@gmail.com>
2023-02-24 23:15:52 +0800

    Fix nav link navigation

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
                     <img src={logo} alt="chaoss-logo" />
                 </div>
                     <ul className='flex-row '>
-                        <li><a href='#' className='nav-link'> Home</a></li>
-                        <li><a href='#' className='nav-link'> About us</a></li>
+                        <li><a href='/' className='nav-link'> Home</a></li>
+                        <li><a href='/about' className='nav-link'> About us</a></li>
                         <li><a href='/event' className='nav-link'> Event Badging</a></li>
                         <li><a href='#' className='nav-link'> Project Badging</a></li>
                     </ul>
