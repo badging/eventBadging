50f6932b6baf149887a7cbec5268c4d1f45c4ca8
c3c1l1a <cesswangumukima@gmail.com>
2023-02-27 17:16:26 +0800

    Add interactivity of the roadmap when hovered and when clicked

--- src/components/roadmap/Roadmap.js
+++ src/components/roadmap/Roadmap.js
@@ -1,24 +1,36 @@
+import { useState } from 'react';
 import { roadmapInfo } from "./roadmap-info";
 import line from "../../assets/images/event-badging/line.png";
 import "./roadmap.css";
 
 
-
-
 export default function Roadmap(){
+  const [activeItem, setActiveItem] = useState({
+    id: 1,
+    status: 'active-item'
+  });
+
+  const onActive = (index) => {
+    setActiveItem({id: index, status:'active-item'});
+  }
+  console.log('test', activeItem);
+
   return (
     <div className="roadmap-section">
       <h2>Badging Roadmap</h2>
       <div className="roadmap">
         <div className="roadmap-screenshot">
             <p>
-                Screenshot of each step as they get hoverd
+                Screenshot of Step {activeItem.id}
             </p>
         </div>
         <div>{
             roadmapInfo.map((item, index) => {
               return (
-                <div className="roadmap-item">
+                <div className={`roadmap-item ${index+1 === activeItem.id ? activeItem.status : ""}`} 
+                      key={index} 
+                      onClick={() => onActive(index+1)}
+                      onMouseOver={() => onActive(index+1)}>
                   <div className="roadmap-item-count">
                     <p>{index + 1}</p>
                   </div>
