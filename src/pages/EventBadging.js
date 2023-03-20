05ff3a6672c15d83f19d7f60acf171b8adb3f06d
c3c1l1a <cesswangumukima@gmail.com>
2023-03-20 20:43:12 +0800

    Refactor the Raodmap into reusable components

--- src/pages/EventBadging.js
+++ src/pages/EventBadging.js
@@ -1,11 +1,20 @@
+import { useState, useEffect } from 'react';
 import { Header, Footer } from "../layouts";
 import EventBadgingHeroSection from "../components/EventBadgingHeroSection";
+import RoadmapSection from '../components/RoadmapSection';
+import Roadmap from '../components/Roadmap';
+import roadmapData from '../components/Roadmap/roadmap-data.json';
 
 const EventBadging = () => {
     return (
         <>
             <Header />
             <EventBadgingHeroSection />
+            <RoadmapSection>
+                <h2>Badging Roadmap</h2>
+                <Roadmap roadmapData={roadmapData}/>
+            </RoadmapSection>               
+
             <Footer />
         </>
     )
