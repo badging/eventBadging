b45181129504b71688b6c19a4c7680e226cc0902
c3c1l1a <cesswangumukima@gmail.com>
2023-03-23 19:12:05 +0800

    Remove the old tabswitcher files

--- src/components/TabSwitcher/header.js
+++
@@ -1,6 +0,0 @@
-function Header({contentItem}) {
-    const header = Object.values(contentItem)[0];
-    return (<h3 className="tab-switcher__content__title"> {header} </h3>);
-}
-
-export default Header;
--- src/components/TabSwitcher/paragraph.js
+++
@@ -1,8 +0,0 @@
-function Paragraph({contentItem}) {
-    const paragraph = Object.values(contentItem)[0];
-    return (<p className="tab-switcher__content__description"> {paragraph} </p>);
-
-    
-}
-
-export default Paragraph;
