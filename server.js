34ccb08a6bf81f2e4f7e4f33cbafda3c9a9bc54d
kaxada <ekaxada@gmail.com>
2023-05-03 12:35:23 +0300

    modified tests and removed redudant file

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-app.use(express.static("build"));
-app.listen(process.env.PORT, function () {
-  console.info(`website running on Port: ${process.env.PORT}`);
-});
