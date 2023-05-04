28192e6455cbbd9ed8106e91abf1adc5b71df319
SkyDe <sky_de1991@outlook.com>
2023-05-04 22:40:33 +0430

    404page design and develope DONE

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -36,6 +36,7 @@ export { default as award } from "./others/award.png";
 export { default as eventBadging } from "./others/event-badging.png";
 export { default as heroOne } from "./others/hero_one.png";
 export { default as heroTwo } from "./others/hero_two.png";
+export { default as error404SVG } from "./others/404.svg";
 
 // testimonial
 export { default as boy } from "./avatars/boy.png";
--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,10 +1,26 @@
 import { Header, Footer } from "../../layouts";
+import { error404SVG } from "../../assets/images";
+import { useNavigate } from "react-router-dom";
+import "./errorPageStyle.scss";
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Header />
-      <h2>404. An Error Occured</h2>
+      <div className="errorPage">
+        <img className="errorPage__image" aria-label="storyset.com/illustration" src={error404SVG} alt="404SVG" />
+        <div className="errorPage__actions">
+          <button
+           className="errorPage__actions--btn"
+           onClick={() => navigate("/")}
+           >Go Home</button>
+          <button
+           className="errorPage__actions--btn"
+           onClick={() => navigate(-1)}
+           >Previous Page</button>
+        </div>
+      </div>
       <Footer />
     </>
   );
