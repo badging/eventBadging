8607d1a14f5d44af4e2a0958ad8aaaca91f34f97
kaxada <ekaxada@gmail.com>
2023-10-12 14:39:27 +0300

    resolved conflicts

--- src/components/SubmitEvent/SubmitEvent.js
+++ src/components/SubmitEvent/SubmitEvent.js
@@ -12,7 +12,6 @@ import {
 
 const SubmitEvent = () => {
   return (
-<<<<<<< HEAD
     <Box
       as="section"
       className="badge-events"
@@ -25,54 +24,46 @@ const SubmitEvent = () => {
         backgroundColor="rgba(7,0,16,0.9)"
       >
         <h2> Submit your Event for a DEI Badge</h2>
-=======
-    <div className="badge-events">
-      <h2> Submit your Event for a DEI Badge</h2>
->>>>>>> 9183e33 (ch(about-page): implement responsiveness)
 
-      <p>
-        The primary motivation to apply for a CHAOSS D&I Badge is the badge
-        itself! The awarded event can show the open source community that they
-        foster healthy D&I practices with a CHAOSS badge.{" "}
-      </p>
-      <div className="buttons">
-        <button>
-          <Link className="link" to="/apply">
-            Apply for a Badge
-          </Link>
-        </button>
-        <button>
-          <Link className="link" to="/badged-events">
-            Badged Events
-          </Link>
-        </button>
-      </div>
-      <p>
-        Applying for a badge supports D&I efforts within an open source
-        community by expressing that your event is willing to improve the ways
-        the work. These efforts can have affects to D&I practices in your
-        event and even outside of your project space. Like these events
-      </p>
-      <div className="logos">
-        <div className="image-container">
-          <img src={chaossWhiteLogo} alt="CHAOSS white logo" />
+        <p>
+          The primary motivation to apply for a CHAOSS D&I Badge is the badge
+          itself! The awarded event can show the open source community that they
+          foster healthy D&I practices with a CHAOSS badge.{" "}
+        </p>
+        <div className="buttons">
+          <button>
+            <Link className="link" to="/apply">
+              Apply for a Badge
+            </Link>
+          </button>
+          <button>
+            <Link className="link" to="/badged-events">
+              Badged Events
+            </Link>
+          </button>
         </div>
-        <div className="image-container">
-          <img src={linuxFoundationLogo} alt="Linux Foundation logo" />
+        <p>
+          Applying for a badge supports D&I efforts within an open source
+          community by expressing that your event is willing to improve the ways
+          the work. These efforts can have affects to D&I practices in your
+          event and even outside of your project space. Like these events
+        </p>
+        <div className="logos">
+          <div className="image-container">
+            <img src={chaossWhiteLogo} alt="CHAOSS white logo" />
+          </div>
+          <div className="image-container">
+            <img src={linuxFoundationLogo} alt="Linux Foundation logo" />
+          </div>
+          <div className="image-container-os">
+            <img src={osSustainAfricaLogo} alt="OSS Africa logo" />
+          </div>
+          <div className="image-container-sca">
+            <img src={sheCodeAfricaLogo} alt="She Code Africa logo" />
+          </div>
         </div>
-        <div className="image-container-os">
-          <img src={osSustainAfricaLogo} alt="OSS Africa logo" />
-        </div>
-        <div className="image-container-sca">
-          <img src={sheCodeAfricaLogo} alt="She Code Africa logo" />
-        </div>
-<<<<<<< HEAD
       </Box>
     </Box>
-=======
-      </div>
-    </div>
->>>>>>> 9183e33 (ch(about-page): implement responsiveness)
   );
 };
 
